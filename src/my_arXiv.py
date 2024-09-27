2ce71d8316c15bf355178fce34adfedd9a024048
Yasushi Nagano <27348322+umigamep@users.noreply.github.com>
2024-09-27 13:20:35 +0000

    feature/search 7 days ago

--- src/my_arXiv.py
+++ src/my_arXiv.py
@@ -1,13 +1,23 @@
-import arxiv
+from datetime import datetime, timedelta
 
-client = arxiv.Client()
+import arxiv
 
 
 def search_arXiv(category, max_result=100):
+    client = arxiv.Client()
+    # 現在の日付から1週間前と2週間前の日付を計算
+    two_weeks_ago = (datetime.now() - timedelta(days=13)).strftime("%Y%m%d")
+    one_week_ago = (datetime.now() - timedelta(days=6)).strftime("%Y%m%d")
+
+    # 日付範囲を含むクエリを作成
+    date_range_query = f"submittedDate:[{two_weeks_ago} TO {one_week_ago}]"
+    full_query = f"cat:{category} AND {date_range_query}"
+
     search = arxiv.Search(
-        query="cat:" + category,
+        query=full_query,
         max_results=max_result,
         sort_by=arxiv.SortCriterion.SubmittedDate,
+        sort_order=arxiv.SortOrder.Descending,
     )
     paper_info_list = [
         {
