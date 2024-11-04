d161de25e20c1d45077ff97b2723820f2a1cf386
umigamep <27348322+umigamep@users.noreply.github.com>
2024-11-04 05:38:14 +0000

    fix/name of flow

--- deploys/agg_arxiv_daily_deploy.py
+++ deploys/agg_arxiv_daily_deploy.py
@@ -6,7 +6,7 @@ SOURCE_REPO = "https://github.com/umigamep/arXiv-db.git"
 if __name__ == "__main__":
     flow.from_source(
         source=SOURCE_REPO,
-        entrypoint="flows/agg_arxiv_daily_flow.py:job",  # Specific flow to run
+        entrypoint="flows/agg_arxiv_daily_flow.py:agg_arxiv_daily",  # Specific flow to run
     ).deploy(
         name="agg_arxiv_daily",
         work_pool_name="my-work-pool",  # Work pool target
