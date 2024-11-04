5446e66e8590b8ac66933ba1d17ce4cb2724c221
umigamep <27348322+umigamep@users.noreply.github.com>
2024-11-04 05:22:15 +0000

    feature/create deployment

---
+++ deploys/agg_arxiv_daily_deploy.py
@@ -0,0 +1,14 @@
+from prefect import flow
+
+# Source for the code to deploy (here, a GitHub repo)
+SOURCE_REPO = "https://github.com/umigamep/arXiv-db.git"
+
+if __name__ == "__main__":
+    flow.from_source(
+        source=SOURCE_REPO,
+        entrypoint="flows/agg_arxiv_daily_flow.py:job",  # Specific flow to run
+    ).deploy(
+        name="agg_arxiv_daily",
+        work_pool_name="my-work-pool",  # Work pool target
+        cron="0 1 * * *",  # Cron schedule (1am every day)
+    )
